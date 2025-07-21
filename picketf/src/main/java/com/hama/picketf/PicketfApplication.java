0131de3751b0c2ea43a42cbbcac986509ca238be
lodehama <lodenx24@gmail.com>
2025-07-21 14:37:54 +0900

    Initial commit

---
+++ picketf/src/main/java/com/hama/picketf/PicketfApplication.java
@@ -0,0 +1,13 @@
+package com.hama.picketf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicketfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicketfApplication.class, args);
+	}
+
+}
---
+++ picketf/src/test/java/com/hama/picketf/PicketfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hama.picketf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicketfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

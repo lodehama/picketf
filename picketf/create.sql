fdebf40b1a44f9fe3d73b824a7262a004afe5cb3
lodehama <lodenx24@gmail.com>
2025-10-30 03:33:28 +0900

    Update create.sql

--- picketf/create.sql
+++ picketf/create.sql
@@ -4,13 +4,14 @@ CREATE TABLE etf (
   etf_code      VARCHAR(12) NOT NULL UNIQUE,        -- 종목코드 (KRX ISU_CD)
   etf_name      VARCHAR(200) NOT NULL,              -- 종목명
   etf_issuer    VARCHAR(100) NOT NULL,              -- 운용사
+  etf_ter 		DECIMAL(5,3),						-- 수수료
   etf_leverage  DECIMAL(3,1) NOT NULL DEFAULT 1.0,  -- 1.0, 2.0, 3.0 ...
   etf_fx_hedged TINYINT NOT NULL DEFAULT 0          -- 0: 비헷지, 1: 환헷지
 );
 
 INSERT INTO etf (etf_category, etf_code, etf_name, etf_issuer, etf_leverage, etf_fx_hedged)
 VALUES
--- ① 일반 1배수 (비헷지)
+-- 일반 1배수 (비헷지)
 ('SNP', '360750', 'TIGER 미국S&P500', '미래에셋자산운용', 1.0, 0),
 ('SNP', '379800', 'KODEX 미국S&P500', '삼성자산운용', 1.0, 0),
 ('SNP', '360200', 'ACE 미국S&P500', '한국투자신탁운용', 1.0, 0),
@@ -21,12 +22,14 @@ VALUES
 ('SNP', '444490', 'WON 미국S&P500', '우리자산운용', 1.0, 0),
 ('SNP', '429760 ', 'PLUS 미국S&P500', '한화자산운용', 1.0, 0),
 
--- ② 일반 1배수 (환헷지형)
+-- 일반 1배수 (환헷지)
 ('SNP', '449180', 'KODEX 미국S&P500(H)', '삼성자산운용', 1.0, 1),
 ('SNP', '448290', 'TIGER 미국S&P500(H)', '미래에셋자산운용', 1.0, 1),
 ('SNP', '453330', 'RISE 미국S&P500(H)', '케이비자산운용', 1.0, 1),
 ('SNP', '269540', 'PLUS 미국S&P500(H)', '한화자산운용', 1.0, 1),
 ('SNP', '449780', 'KIWOOM 미국S&P500(H)', '키움투자자산운용', 1.0, 1),
 
--- ③ 레버리지 2배 (비헷지)
-('SNP', '225040', 'TIGER 미국S&P500레버리지(합성 H)', '미래에셋자산운용', 2.0, 0);
+-- 레버리지 2배 (비헷지)
+
+-- 레버리지 2배 (환헷지)
+('SNP', '225040', 'TIGER 미국S&P500레버리지(합성 H)', '미래에셋자산운용', 2.0, 1);

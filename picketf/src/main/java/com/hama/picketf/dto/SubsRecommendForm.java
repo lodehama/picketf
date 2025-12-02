cff27c8b2358208b54641e0ba89b68552a35fe78
lodehama <lodenx24@gmail.com>
2025-12-03 05:59:46 +0900

    결제일 추가

--- picketf/src/main/java/com/hama/picketf/dto/SubsRecommendForm.java
+++ picketf/src/main/java/com/hama/picketf/dto/SubsRecommendForm.java
@@ -7,4 +7,6 @@ public class SubsRecommendForm {
   private String name; // 구독 이름 (넷플릭스, 통신비 ...)
   private Integer price; // 사용자가 수정한 가격
   private String type; // 지출 타입 (생활/콘텐츠/음악 등)
+  private Integer billingDay;
+
 }
--- picketf/src/main/java/com/hama/picketf/service/SubsService.java
+++ picketf/src/main/java/com/hama/picketf/service/SubsService.java
@@ -15,28 +15,28 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SubsService {
 
-    private final SubsDAO subsDAO;
+  private final SubsDAO subsDAO;
 
-    public void addRecommendedSubs(Long userNum, List<SubsRecommendForm> forms) {
-        LocalDate today = LocalDate.now();
+  public void addRecommendedSubs(Long userNum, List<SubsRecommendForm> forms) {
+    LocalDate today = LocalDate.now();
 
-        List<SubsDTO> list = forms.stream()
-                .map(f -> {
-                    SubsDTO dto = new SubsDTO();
-                    dto.setSubsUsNum(userNum);
-                    dto.setSubsName(f.getName());
-                    dto.setSubsPrice(f.getPrice());
-                    dto.setSubsType(f.getType());
-                    dto.setSubsBillingDay(null); // 필요하면 기본값 설정
-                    dto.setSubsActive(true);
-                    dto.setSubsStartDate(today);
-                    dto.setSubsIcon(null); // 나중에 아이콘 쓰면 채우기
-                    return dto;
-                })
-                .toList();
+    List<SubsDTO> list = forms.stream()
+        .map(f -> {
+          SubsDTO dto = new SubsDTO();
+          dto.setSubsUsNum(userNum);
+          dto.setSubsName(f.getName());
+          dto.setSubsPrice(f.getPrice());
+          dto.setSubsType(f.getType());
+          dto.setSubsBillingDay(f.getBillingDay());
+          dto.setSubsActive(true);
+          dto.setSubsStartDate(today);
+          dto.setSubsIcon(null); // 나중에 아이콘 쓰면 채우기
+          return dto;
+        })
+        .toList();
 
-        if (!list.isEmpty()) {
-            subsDAO.insertSubsList(list);
-        }
+    if (!list.isEmpty()) {
+      subsDAO.insertSubsList(list);
     }
+  }
 }

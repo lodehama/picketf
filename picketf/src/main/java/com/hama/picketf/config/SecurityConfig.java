0e6aa3f0cf3dabed2a08f4af5616d056d19e482e
lodehama <lodenx24@gmail.com>
2025-12-03 08:24:29 +0900

    Update SecurityConfig.java

--- picketf/src/main/java/com/hama/picketf/config/SecurityConfig.java
+++ picketf/src/main/java/com/hama/picketf/config/SecurityConfig.java
@@ -15,46 +15,45 @@ import com.hama.picketf.service.UserDetailService;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
-	@Autowired
-	private UserDetailService memberDetailService;
-	
-	@Value("${security.rememberme.key}")
-	private String rememberMeKey;
+  @Autowired
+  private UserDetailService memberDetailService;
 
-	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf ->csrf.disable())
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
-                .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
-                .anyRequest().permitAll()  // 그 외 요청은 인증 필요
-            )
-            .formLogin((form) -> form
-                //.loginPage("/login")  // 커스텀 로그인 페이지 설정
-                .permitAll()           // 로그인 페이지는 접근 허용
-                .loginProcessingUrl("/login")//
-                .defaultSuccessUrl("/")
-            )
-            //자동 로그인 처리
-						.rememberMe(rm-> rm
-							.userDetailsService(memberDetailService)//자동 로그인할 때 사용할 userDetailService를 추가
-							.key(rememberMeKey)//키가 변경되면 기존 토큰이 무효처리
-							.rememberMeCookieName("rememberme")//쿠키 이름
-							.tokenValiditySeconds(60 * 60 * 24 * 300)//유지 기간 : 300일
-						)
-			      .logout((logout) -> logout
-            		.logoutUrl("/logout")
-            		.logoutSuccessUrl("/")
-            		.clearAuthentication(true)
-            		.invalidateHttpSession(true)
-            		.permitAll());  // 로그아웃도 모두 접근 가능
-        return http.build();
-    }
+  @Value("${security.rememberme.key}")
+  private String rememberMeKey;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
+            .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
+            .anyRequest().permitAll() // 그 외 요청은 전부 인증 없이 허용
+        )
+        .formLogin((form) -> form
+            // .loginPage("/login") // 커스텀 로그인 페이지 설정
+            .permitAll() // 로그인 페이지는 접근 허용
+            .loginProcessingUrl("/login")//
+            .defaultSuccessUrl("/"))
+        // 자동 로그인 처리
+        .rememberMe(rm -> rm
+            .userDetailsService(memberDetailService)// 자동 로그인할 때 사용할 userDetailService를 추가
+            .key(rememberMeKey)// 키가 변경되면 기존 토큰이 무효처리
+            .rememberMeCookieName("rememberme")// 쿠키 이름
+            .tokenValiditySeconds(60 * 60 * 24 * 300)// 유지 기간 : 300일
+        )
+        .logout((logout) -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")
+            .clearAuthentication(true)
+            .invalidateHttpSession(true)
+            .permitAll()); // 로그아웃도 모두 접근 가능
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }

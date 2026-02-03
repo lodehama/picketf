e7c50ff4ea3898460e223213082a1dac30401960
lodehama <lodenx24@gmail.com>
2026-02-04 03:47:24 +0900

    뼈대 만들기

--- picketf/src/main/java/com/hama/picketf/controller/PortfolioController.java
+++ picketf/src/main/java/com/hama/picketf/controller/PortfolioController.java
@@ -2,6 +2,7 @@ package com.hama.picketf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,4 +18,11 @@ public class PortfolioController {
   public String add() {
     return "portfolio-add";
   }
+
+  @PostMapping("/add")
+  public String addSubmit() {
+    // 로직 추가예정
+    return "redirect:/portfolio";
+  }
+
 }

81c73626a0229471bd02ab4bc2c81e6d81bbe8fe
lodehama <lodenx24@gmail.com>
2026-02-05 07:28:15 +0900

    url isa로 변경

--- picketf/src/main/java/com/hama/picketf/controller/IsaController.java
+++ picketf/src/main/java/com/hama/picketf/controller/IsaController.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/portfolio")
-public class PortfolioController {
+@RequestMapping("/isa")
+public class IsaController {
 
   @GetMapping
-  public String portfolio() {
-    return "portfolio";
+  public String isa() {
+    return "isa";
   }
 
   @GetMapping("/add")
   public String add() {
-    return "portfolio-add";
+    return "isa-add";
   }
 
   @PostMapping("/add")
   public String addSubmit() {
     // 로직 추가예정
-    return "redirect:/portfolio";
+    return "redirect:/isa";
   }
 
 }

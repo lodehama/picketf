dee434291670847951fcf1e453a3c208529773ec
lodehama <lodenx24@gmail.com>
2026-02-10 08:18:57 +0900

    csrf 문제 해결

--- picketf/src/main/java/com/hama/picketf/config/SecurityConfig.java
+++ picketf/src/main/java/com/hama/picketf/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 import com.hama.picketf.model.util.UserRole;
 import com.hama.picketf.service.UserDetailService;
@@ -26,31 +27,36 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf
-        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-        .authorizeHttpRequests((requests) -> requests
+    CookieCsrfTokenRepository repo = CookieCsrfTokenRepository.withHttpOnlyFalse();
+    repo.setHeaderName("X-XSRF-TOKEN"); // csrf 토큰 헤더 이름 변경
+
+    // XOR 마스킹 말고, "원본 토큰"을 헤더로 받도록 고정
+    CsrfTokenRequestAttributeHandler handler = new CsrfTokenRequestAttributeHandler();
+
+    http
+        .csrf(csrf -> csrf.csrfTokenRepository(repo)
+            .csrfTokenRequestHandler(handler) // CSRF 토큰을 헤더에서 읽도록 설정
+        )
+        .authorizeHttpRequests(req -> req
             .requestMatchers("/post/insert/*").hasAuthority(UserRole.USER.name())
             .requestMatchers("/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
-            .anyRequest().permitAll() // 그 외 요청은 전부 인증 없이 허용
-        )
-        .formLogin((form) -> form
-            // .loginPage("/login") // 커스텀 로그인 페이지 설정
-            .permitAll() // 로그인 페이지는 접근 허용
-            .loginProcessingUrl("/login")//
+            .anyRequest().permitAll())
+        .formLogin(form -> form
+            .permitAll()
+            .loginProcessingUrl("/login")
             .defaultSuccessUrl("/"))
-        // 자동 로그인 처리
         .rememberMe(rm -> rm
-            .userDetailsService(memberDetailService)// 자동 로그인할 때 사용할 userDetailService를 추가
-            .key(rememberMeKey)// 키가 변경되면 기존 토큰이 무효처리
-            .rememberMeCookieName("rememberme")// 쿠키 이름
-            .tokenValiditySeconds(60 * 60 * 24 * 300)// 유지 기간 : 300일
-        )
-        .logout((logout) -> logout
+            .userDetailsService(memberDetailService)
+            .key(rememberMeKey)
+            .rememberMeCookieName("rememberme")
+            .tokenValiditySeconds(60 * 60 * 24 * 300))
+        .logout(logout -> logout
             .logoutUrl("/logout")
             .logoutSuccessUrl("/")
             .clearAuthentication(true)
             .invalidateHttpSession(true)
-            .permitAll()); // 로그아웃도 모두 접근 가능
+            .permitAll());
+
     return http.build();
   }
 

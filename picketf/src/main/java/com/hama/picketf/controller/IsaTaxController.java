bf9c778c3661c086c58298a55219009d9e8580d5
lodehama <lodenx24@gmail.com>
2026-02-17 21:20:51 +0900

    컨트롤러 코드 변경

--- picketf/src/main/java/com/hama/picketf/controller/IsaTaxController.java
+++ picketf/src/main/java/com/hama/picketf/controller/IsaTaxController.java
@@ -2,11 +2,14 @@ package com.hama.picketf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/isa/tax")
 public class IsaTaxController {
-  @GetMapping("/isa/tax")
-  public String isatax() {
+
+  @GetMapping
+  public String page() {
     return "isa-tax";
   }
 
